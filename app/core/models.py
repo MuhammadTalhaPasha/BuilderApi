cb98deccd65dbc67023475e4e1e65cc11e30f3c4
talhapasha81 <talhapasha81@gmail.com>
2020-03-07 17:45:40 +0500

    added database support

---
+++ app/core/models.py
@@ -0,0 +1,91 @@
+import uuid
+import os
+
+from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, \
+    PermissionsMixin
+
+from django.conf import settings
+
+
+# def userfile_file_path(instance, filename):
+#     """generate file_path for new userfile file"""
+#     ext = filename.split('.')[-1]
+#     filename = f'{uuid.uuid4()}.{ext}'
+#
+#     return os.path.join('uploads/user_files', filename)
+#
+
+class UserManager(BaseUserManager):
+
+    def create_user(self, email, password=None, **extra_fields):
+        """Creates and saves a new User"""
+        if not email:
+            raise ValueError('User must have email address')
+        user = self.model(email=self.normalize_email(email), **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+
+        return user
+
+    def create_superuser(self, email, password):
+        """create and a new save super user"""
+        user = self.create_user(email, password)
+        user.is_staff = True
+        user.is_superuser = True
+        user.save(using=self.db)
+
+        return user
+
+
+class User(AbstractBaseUser, PermissionsMixin):
+    """Custom user model that supports using email instead of username"""
+    email = models.EmailField(max_length=255, unique=True)
+    name = models.CharField(max_length=255)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+
+    objects = UserManager()
+
+    USERNAME_FIELD = 'email'
+
+#
+# class Tag(models.Model):
+#     """tag to be used for a user_file"""
+#     name = models.CharField(max_length=255)
+#     user = models.ForeignKey(
+#         settings.AUTH_USER_MODEL,
+#         on_delete=models.CASCADE
+#     )
+#
+#     def __str__(self):
+#         return self.name
+#
+#
+# class File_type(models.Model):
+#     """File_type to be used for fule"""
+#     type = models.CharField(max_length=255)
+#     user = models.ForeignKey(
+#         settings.AUTH_USER_MODEL,
+#         on_delete=models.CASCADE
+#     )
+#
+#     def __str__(self):
+#         return self.type
+#
+#
+# class User_File(models.Model):
+#     """user_files object"""
+#     user = models.ForeignKey(
+#         settings.AUTH_USER_MODEL,
+#         on_delete=models.CASCADE
+#     )
+#     title = models.CharField(max_length=255)
+#     created_on = models.DateTimeField(auto_now_add=True)
+#     link = models.CharField(max_length=255, blank=True)
+#     file_types = models.ManyToManyField('File_type')
+#     tags = models.ManyToManyField('Tag')
+#     file = models.FileField(null=True, upload_to=userfile_file_path)
+#
+#     def __str__(self):
+#         return self.title
---
+++ app/core/tests/test_model.py
@@ -0,0 +1,87 @@
+from unittest.mock import patch
+
+from django.test import TestCase
+from django.contrib.auth import get_user_model
+
+from core import models
+
+
+def sample_user(email='test@pashadev.com', password='test123'):
+    """create a sample_user"""
+    return get_user_model().objects.create_user(email, password)
+
+
+class ModelTests(TestCase):
+
+    def test_create_user_with_email_successfull(self):
+        """test creating a new user with email is
+        test_create_user_with_email_successfull"""
+        email = 'test@pashadev.com'
+        password = 'testpass123'
+        user = get_user_model().objects.create_user(
+            email=email,
+            password=password
+        )
+
+        self.assertEqual(user.email, email)
+        self.assertTrue(user.check_password(password))
+
+    def test_new_user_email_normalized(self):
+        """Test the email for a new user is normalized"""
+        email = 'test@PASHADEV.com'
+        user = get_user_model().objects.create_user(email, 'test123')
+
+        self.assertEqual(user.email, email.lower())
+
+    def test_new_user_invalid_email(self):
+        """test createing uesr with no email raises"""
+        with self.assertRaises(ValueError):
+            get_user_model().objects.create_user(None, 'test123')
+
+    def test_new_superuser(self):
+        """Test creating a new superuser"""
+        user = get_user_model().objects.create_superuser(
+            'test@londonappdev.com',
+            'test123'
+        )
+
+        self.assertTrue(user.is_superuser)
+        self.assertTrue(user.is_staff)
+    #
+    # def test_tag_str(self):
+    #     """Test the tag string representation"""
+    #     tag = models.Tag.objects.create(
+    #         user=sample_user(),
+    #         name='Vegan'
+    #     )
+    #
+    #     self.assertEqual(str(tag), tag.name)
+    #
+    # def test_file_type_str(self):
+    #     """test the file_type string representation"""
+    #     file_type = models.File_type.objects.create(
+    #         user=sample_user(),
+    #         type='DXF'
+    #     )
+    #
+    #     self.assertEqual(str(file_type), file_type.type)
+    #
+    # def test_uesr_file_str(self):
+    #     """tests the user_files string representation"""
+    #     user_file = models.User_File.objects.create(
+    #         user=sample_user(),
+    #         title='file room',
+    #         created_on='23/5/19'
+    #     )
+    #
+    #     self.assertEqual(str(user_file), user_file.title)
+    #
+    # @patch('uuid.uuid4')
+    # def test_userfile_file_name_uuid(self, mock_uuid):
+    #     """Test that image/file is saved in the correct location"""
+    #     uuid = 'test-uuid'
+    #     mock_uuid.return_value = uuid
+    #     file_path = models.userfile_file_path(None, 'myimage.dxf')
+    #
+    #     exp_path = f'uploads/user_files/{uuid}.dxf'
+    #     self.assertEqual(file_path, exp_path)
